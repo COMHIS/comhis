ca5c3f2b1464d0b17f1692c86b2346a9c2414fad
antagomir <leo.lahti@iki.fi>
2021-01-15 15:49:22 +0200

    ok

--- R/gatherings_table.R
+++ R/gatherings_table.R
@@ -9,7 +9,7 @@
 #' @keywords utilities
 gatherings_table <- function (...) {
 
-  f <- system.file("extdata/document_size_abbreviations.csv", package = "fennica")
+  f <- system.file("extdata/document_size_abbreviations.csv", package = "comhis")
   dd <- read.csv(f, header = TRUE, sep = ";")
   dd <- apply(dd, 2, as.character)
   dd[dd == ""] <- NA
